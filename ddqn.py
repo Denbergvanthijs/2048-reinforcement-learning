d98b93991917a07b7b9c7150ea1d688b326b7adc
denbergvanthijs <denbergvanthijs@gmail.com>
2020-12-24 17:19:22 +0100

    Added plotting of trained model

--- ddqn.py
+++ ddqn.py
@@ -1,21 +1,26 @@
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 from tf_agents.agents.dqn.dqn_agent import DdqnAgent
+from tf_agents.drivers.dynamic_step_driver import DynamicStepDriver
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
 from tf_agents.networks.q_network import QNetwork
 from tf_agents.policies.random_tf_policy import RandomTFPolicy
 from tf_agents.replay_buffers.tf_uniform_replay_buffer import \
     TFUniformReplayBuffer
-from tf_agents.trajectories import trajectory
 from tf_agents.utils import common
 
 from environment import GameEnv
 
 num_iterations = 20_000
-
-initial_collect_steps = 1_000
+warmup_steps = 10_000
 collect_steps_per_iteration = 1
 replay_buffer_max_length = 10_000
+target_update_period = 500
+decay_episodes = 5_000
+min_epsilon = 0.1
+gamma = 0.95
 
 batch_size = 32
 learning_rate = 0.001
@@ -29,6 +34,7 @@ train_env = TFPyEnvironment(env)
 eval_env = TFPyEnvironment(env)
 fc_layer_params = (40, 40, )
 train_step_counter = tf.Variable(0)
+epsilon_greedy = tf.compat.v1.train.polynomial_decay(1.0, train_step_counter, decay_episodes, end_learning_rate=min_epsilon)
 
 q_net = QNetwork(train_env.observation_spec(),
                  train_env.action_spec(),
@@ -39,7 +45,10 @@ agent = DdqnAgent(train_env.time_step_spec(),
                   q_network=q_net,
                   optimizer=Adam(learning_rate=learning_rate),
                   td_errors_loss_fn=common.element_wise_squared_loss,
-                  train_step_counter=train_step_counter)
+                  train_step_counter=train_step_counter,
+                  epsilon_greedy=epsilon_greedy,
+                  gamma=gamma,
+                  target_update_period=target_update_period)
 
 agent.initialize()
 agent.train = common.function(agent.train)
@@ -68,23 +77,20 @@ replay_buffer = TFUniformReplayBuffer(data_spec=agent.collect_data_spec,
                                       batch_size=train_env.batch_size,
                                       max_length=replay_buffer_max_length)
 
+warmup_driver = DynamicStepDriver(train_env,
+                                  random_policy,
+                                  observers=[replay_buffer.add_batch],
+                                  num_steps=warmup_steps)
 
-def collect_step(environment, policy, buffer):
-    time_step = environment.current_time_step()
-    action_step = policy.action(time_step)
-    next_time_step = environment.step(action_step.action)
-    traj = trajectory.from_transition(time_step, action_step, next_time_step)
+collect_driver = DynamicStepDriver(train_env,
+                                   agent.collect_policy,
+                                   observers=[replay_buffer.add_batch],
+                                   num_steps=collect_steps_per_iteration)
 
-    # Add trajectory to the replay buffer
-    buffer.add_batch(traj)
+warmup_driver.run = common.function(warmup_driver.run)
+collect_driver.run = common.function(collect_driver.run)
 
-
-def collect_data(env, policy, buffer, steps):
-    for _ in range(steps):
-        collect_step(env, policy, buffer)
-
-
-collect_data(train_env, random_policy, replay_buffer, initial_collect_steps)
+warmup_driver.run(time_step=None, policy_state=random_policy.get_initial_state(train_env.batch_size))
 
 dataset = replay_buffer.as_dataset(num_parallel_calls=3,
                                    sample_batch_size=batch_size,
@@ -94,17 +100,39 @@ iterator = iter(dataset)
 avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
 returns = [avg_return]
 
+ts = None
+policy_state = agent.collect_policy.get_initial_state(train_env.batch_size)
 for _ in range(num_iterations):
-    collect_data(train_env, agent.collect_policy, replay_buffer, collect_steps_per_iteration)
+    ts, policy_state = collect_driver.run(time_step=ts, policy_state=policy_state)
 
     experience, _ = next(iterator)
     train_loss = agent.train(experience).loss
     step = agent.train_step_counter.numpy()
 
     if step % log_interval == 0:
-        print(f"{step=}; train loss={train_loss.numpy():.6f}")
+        print(f"{step=}; train loss={train_loss.numpy():.2f}")
 
     if step % eval_interval == 0:
         avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
         print(f"{step=}; {avg_return=}")
         returns.append(avg_return)
+
+time_step = eval_env.reset()
+episode_return = 0.0
+fig, ax = plt.subplots()
+ims = []
+actions = {0: "Up", 1: "Left", 2: "Down", 3: "Right"}
+while not time_step.is_last():
+    action_step = agent.policy.action(time_step)
+    time_step = eval_env.step(action_step.action)
+    line = plt.imshow(time_step.observation.numpy().reshape((4, 4)), animated=True)
+    episode_return += time_step.reward
+
+    text = f"Action: {actions.get(action_step.action.numpy()[0])}; Score: {episode_return}"
+    title = ax.text(0.5, 1.05, text, size=plt.rcParams["axes.titlesize"], ha="center", transform=ax.transAxes)
+    ims.append([line, title])
+
+plt.rcParams['animation.ffmpeg_path'] = "D:/ffmpeg/bin/ffmpeg.exe"
+ani = animation.ArtistAnimation(fig, ims, interval=200, repeat=True, blit=False, repeat_delay=1000)
+ani.save("2048trained.gif")  # Can take a few seconds
+plt.show()
